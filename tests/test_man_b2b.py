a91f6397fc1240de21c5224661d9f13dcc4761f3
Marcin <marcin.konow@lczyk.xyz>
2024-09-03 13:15:00 +0100

    b2b test

---
+++ tests/test_man_b2b.py
@@ -0,0 +1,28 @@
+import pytest
+
+from xpc import Manager
+
+
+def start_man() -> Manager:
+    man: Manager | None = Manager(
+        address=("localhost", 9999),
+        authkey="password",
+    )
+    try:
+        assert man
+        man.start(timeout=1.0)
+    except ConnectionRefusedError:
+        man = None
+    if not man:
+        pytest.fail("Failed to connect to the server.", pytrace=False)
+    return man
+
+
+def test_man_1() -> None:
+    man = start_man()
+    man.shutdown()
+
+
+def test_man_2() -> None:
+    man = start_man()
+    man.shutdown()
